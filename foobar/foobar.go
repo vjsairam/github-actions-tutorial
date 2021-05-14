e2cf0095652d4f2432eb3c0ba228bae17f1ee240
Sairam <sairam.vj@tokigames.com>
2021-05-14 19:02:18 +0530

    Adding Secret scanning

--- foobar/foobar.go
+++ foobar/foobar.go
@@ -22,7 +22,7 @@ func Sequence(length int) ([]string, error) {
 			seq[i] = "foobar"
 		case n%5 == 0:
 			seq[i] = "bar"
-			private_key_id = "d30b0f8858589d3d1294aee5b"
+			//private_key_id = "d30b0f8858589d3d1294aee5b"
 		case n%3 == 0:
 			seq[i] = "foo"
 		default:
